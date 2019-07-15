cc1275d737f0dacbb456c60f783a7360900e6e8c
Ben Schmoker <bschmoker@salesforce.com>
2019-07-15 15:02:38 -0700

    add

--- fancier_terra/main.tf
+++ fancier_terra/main.tf
@@ -7,8 +7,11 @@ resource "aws_instance" "app" {
   ami = "ami-0cfee17793b08a293"
   instance_type = "t2.micro"
 
-  # NOTE: the variable definition file named "terraform.tvars" is auto loaded
+  # NOTE: the variable definition file named "terraform.tvars" is auto loaded by `plan`
   # if otherwise named, you must define it via -var-file
+
+  # NOTE when deploying, first run `plan -var-file prod.tfvars -out prod` to emit a config with that state
+  # then run `apply prod` to deploy
 
   # create two vms, one in each sub-zone of nova
   count = "${var.total_boxes}"
